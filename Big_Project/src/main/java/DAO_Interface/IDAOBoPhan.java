36ed9e1020eaefb560baec1e715082e95557acaf
TranVanHung_20061461 <hungtran2002a2@gmail.com>
2024-04-17 23:03:18 +0700

    JPA_v3

--- Big_Project/src/main/java/DAO_Interface/IDAOBoPhan.java
+++ Big_Project/src/main/java/DAO_Interface/IDAOBoPhan.java
@@ -1,6 +1,19 @@
 package DAO_Interface;
 
+import ptud.Entity.BoPhan;
+
 import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.ArrayList;
 
 public interface IDAOBoPhan extends Remote {
+    public BoPhan get(String id) throws RemoteException;
+    public ArrayList<BoPhan> getAll() throws RemoteException;
+    public boolean insert(BoPhan boPhan) throws RemoteException;
+    public boolean update(BoPhan boPhan) throws RemoteException;
+    public boolean deleteById(String id) throws RemoteException;
+    public static String getMaBoPhanByTenBoPhan(String tenBoPhan) throws RemoteException {
+        return null;
+    }
+    public ArrayList<BoPhan> filter(String prefix) throws RemoteException;
 }
---
+++ Big_Project/src/main/java/DAO_Interface/IDAOCongDoan.java
@@ -0,0 +1,23 @@
+package DAO_Interface;
+
+import ptud.Entity.CongDoan;
+
+import java.rmi.Remote;
+import java.util.ArrayList;
+
+public interface IDAOCongDoan extends Remote {
+    public CongDoan get(String id) throws Exception;
+    public ArrayList<CongDoan> getAll() throws Exception;
+    public ArrayList<CongDoan> getAllByMaSP(String maSP) throws Exception;
+    public ArrayList<CongDoan> getAllByMaBP(String maBP) throws Exception;
+    public ArrayList<CongDoan> getAllByTrangThai(boolean trangThai) throws Exception;
+    public boolean insert(CongDoan t) throws Exception;
+    public boolean update(CongDoan t) throws Exception;
+    public boolean deleteById(String id) throws Exception;
+    public String getLastMaCD(String maSP) throws Exception;
+    public ArrayList<String> getDsCDHQ(String maCDTQ) throws Exception;
+    public int getSoLuongHoanThanh(String maCD) throws Exception;
+    public ArrayList<CongDoan> getDsCDTQ(String maCD) throws Exception;
+    public int getSoLuongChuanBi(String maCD) throws Exception;
+    public void updateSoLuong( String maCD, int delta, boolean giam ) throws Exception;
+}
