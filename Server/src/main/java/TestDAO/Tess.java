bdb8496283e154e8ecc05877b414b641fe028a61
phucnguyennnn <20117141.phuc@student.iuh.edu.vn>
2024-04-22 16:24:22 +0700

    tests

---
+++ Server/src/main/java/TestDAO/Tess.java
@@ -0,0 +1,18 @@
+package TestDAO;
+
+import DAO_Implement.DAOPhieuChamCongCongNhan;
+
+public class Tess {
+	public static void main(String[] args) {
+		try {
+			DAOPhieuChamCongCongNhan daoPhieuChamCongCongNhan = new DAOPhieuChamCongCongNhan();
+			System.out.print("tétcc");
+		    Object[] aObjects = daoPhieuChamCongCongNhan.getThongTinChamCongByMaCongNhan("SX01051001");
+		    System.out.print("cc"+aObjects[0]);
+		} catch (Exception e) {
+			System.out.print("cc"+e);
+			// TODO: handle exception
+		}
+		
+	}
+}
